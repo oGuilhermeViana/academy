f09ea0e2e9cf444e99928844e9907880602815fe
Guilherme Viana <guilhermeviana1212@gmail.com>
2022-11-04 18:51:28 -0300

    add bootstrap e jquery com webjars

--- src/main/java/com/oguilhermeviana/academy/academy/controllers/HomeController.java
+++ src/main/java/com/oguilhermeviana/academy/academy/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class PageController {
+public class HomeController {
 
   @GetMapping("/")
   public ModelAndView home() {
