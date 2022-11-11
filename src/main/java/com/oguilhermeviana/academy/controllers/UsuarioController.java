e4de5189bded1ce55629e247d7600825c8f914ff
Guilherme Viana <guilhermeviana1212@gmail.com>
2022-11-11 16:25:32 -0300

    logout

--- src/main/java/com/oguilhermeviana/academy/controllers/UsuarioController.java
+++ src/main/java/com/oguilhermeviana/academy/controllers/UsuarioController.java
@@ -73,4 +73,11 @@ public class UsuarioController {
 
     return mv;
   }
+
+  @PostMapping("/logout")
+  public ModelAndView logout(HttpSession session) {
+    session.invalidate();
+    return login();
+  }
+
 }
